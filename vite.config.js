f0c8c344aae6aa95bcbdfb636632b636b1740aab
kannan V.S. <vskannan4135@gmail.com>
2024-09-23 11:18:44 +0530

    Updated vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/ecommerce-react-vite/"
+  base: "/ecommerce-react-vite/", // GitHub Pages base path
+  
+  // Build optimizations
+  build: {
+    rollupOptions: {
+      output: {
+        // Manually split large chunks to optimize performance
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    
+    // Increase chunk size warning limit if needed
+    chunkSizeWarningLimit: 500, // Set to a higher limit if you expect larger chunks
+  }
 })
