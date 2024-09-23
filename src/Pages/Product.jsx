bbce95be2d1328b81d4458abcca747573ff11a0d
kannan V.S. <vskannan4135@gmail.com>
2024-09-23 11:12:37 +0530

    Initial push for vite-ecommerce project

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -29,7 +29,7 @@ const Product = () => {
   }, [productId])
 
   return productData ? (
-    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100 mt-6'>
 
       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>  {/* -------- Product Row ---------- */}
 
